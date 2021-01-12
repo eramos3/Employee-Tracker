24b3c5d0797f65c0d87c321274dc143519b73361
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-12 15:14:00 -0800

    added departments

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
 const inquirer = require("inquirer");
-const cTable = require('console.table');
-// get the client
-const mysql = require('mysql2');
-
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '!Cheesydata1',
-    database: 'company',
-});
-
-connection.connect(err => {
-    if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    console.log('Welcome to Employee Tracker');
-    promptUser();
-});
-
+const viewDepartments = require("./utils/queries");
 
 promptUser = () => {
     return inquirer.prompt([
---
+++ utils/queries.js
@@ -0,0 +1,35 @@
+const cTable = require('console.table');
+// get the client
+const mysql = require('mysql2');
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '!Cheesydata1',
+    database: 'company',
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    console.log('Welcome to Employee Tracker');
+    promptUser();
+});
+// queries 
+const viewDepartments = () => {
+    console.log('----------------------');
+    console.log('Showing Departments...');
+    console.log('----------------------');
+
+    connection.query(
+        'SELECT * FROM departments',
+        function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            promptUser();
+        }
+    );
+};
+
+module.exports = viewDepartments;
