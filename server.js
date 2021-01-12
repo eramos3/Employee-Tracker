6c79d4701b411da200ba1a3cd4b0410a6bbc6e87
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-12 15:01:44 -0800

    added inquirer

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// const connection = require('./db/employees.db');
+const inquirer = require("inquirer");
 const cTable = require('console.table');
 // get the client
 const mysql = require('mysql2');
-const inquirer = require("inquirer");
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -17,4 +16,51 @@ connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     console.log('Welcome to Employee Tracker');
+    promptUser();
 });
+
+
+promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select an option.',
+            name: 'action',
+            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Update Employee Manager']
+        }
+    ])
+        .then(choice => {
+
+            if (choice.action === 'View Departments') {
+                viewDepartments();
+            }
+
+            if (choice.action === 'View Roles') {
+                viewRoles();
+            }
+
+            if (choice.action === 'View Employees') {
+                viewEmployees();
+            }
+
+            if (choice.action === 'Add Department') {
+                addDepartment();
+            }
+
+            if (choice.action === 'Add Role') {
+                addRole();
+            }
+
+            if (choice.action === 'Add Employee') {
+                addEmployee();
+            }
+
+            if (choice.action === 'Update Employee Role') {
+                updateEmployeeRole();
+            }
+
+            if (choice.action === "Update Employee Manager") {
+                updateEmployeeManager();
+            }
+        });
+};
