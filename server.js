1cc7b069eb006bee775e4cfdc471042c26c1838f
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-12 17:24:06 -0800

    added ability to add role

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const {viewDepartments,viewRoles} = require("./utils/queries");
+const {viewDepartments,viewRoles,addDepartment,addRole} = require("./utils/queries");
 
 promptUser = () => {
     return inquirer.prompt([
@@ -7,7 +7,7 @@ promptUser = () => {
             type: 'list',
             message: 'Please select an option.',
             name: 'action',
-            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Update Employee Manager, Delete Employee']
+            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', 'Delete Employee']
         }
     ])
         .then(choice => {
--- utils/queries.js
+++ utils/queries.js
@@ -1,6 +1,8 @@
 const cTable = require('console.table');
 // get the client
 const mysql = require('mysql2');
+const inquirer = require("inquirer");
+
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -47,4 +49,98 @@ const viewRoles = () => {
     );
 };
 
-module.exports = { viewDepartments, viewRoles };
+
+const addDepartment = () => {
+    console.log('---------------------');
+    console.log('Adding New Department');
+    console.log('---------------------');
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'Type in department name',
+                validate: department => {
+                    if (department) {
+                        return true;
+                    } else {
+                        console.log("Enter department name");
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(nDepartment => {
+
+            connection.query(
+                'INSERT INTO departments SET ?', nDepartment,
+
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`<----- New Department has been added as id = ${res.insertId} ----->`);
+                    viewDepartments();
+                }
+            );
+        })
+};
+
+const addRole = () => {
+    console.log('---------------');
+    console.log('Adding New Role');
+    console.log('---------------');
+
+    return inquirer
+        .prompt([
+            {
+                type: 'number',
+                name: 'department_id',
+                message: 'Type in the department ID in which the role belongs to',
+                validate: deptId => {
+                    if (deptId) {
+                        return true;
+                    } else {
+                        console.log("Enter department ID");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'role_title',
+                message: 'Type in the role title',
+                validate: role => {
+                    if (role) {
+                        return true;
+                    } else {
+                        console.log("Enter role title");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'Type in the salary for this role title',
+                validate: salary => {
+                    if (salary) {
+                        return true;
+                    } else {
+                        console.log("Please enter a salary");
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(newRole => {
+            connection.query(
+                'INSERT INTO roles SET ?', newRole,
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(`<----- New Role has been added as id = ${res.insertId} ----->`);
+                    viewRoles();
+                }
+            );
+        })
+};
+module.exports = { viewDepartments, viewRoles, addDepartment,addRole };
