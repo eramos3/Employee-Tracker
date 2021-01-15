2e4cd7f2f5f3e1512865a83217ef2f486a411328
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-15 08:26:58 -0800

    added env file

--- utils/queries.js
+++ utils/queries.js
@@ -1,13 +1,14 @@
 const cTable = require('console.table');
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
-
+require('dotenv').config();
+const processs = require("process");
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '!Cheesydata1',
+    password: process.env.password,
     database: 'company',
 });
 
